0e2d6ccafc7fd28a59bbd3a1701c3807e8249475
Iamlixinzhao <150439580+Iamlixinzhao@users.noreply.github.com>
2025-05-26 21:37:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from lefdef import C_LefReader
 from lefdef import C_DefReader
 
 lef_reader = C_LefReader()
-_lef = lef_reader.read("/home/alaie/Nangate.lef")
+_lef = lef_reader.read("external/def/TEST/complete.5.8.def")
 _lef.print()
 
 def_reader = C_DefReader()
-_def = def_reader.read("/home/alaie/bsg_chip.def")
+_def = def_reader.read("external/lef/TEST/complete.5.8.lef")
 _def.print()
