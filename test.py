7f69947544201462d39fbf0d6d19dd6188cfad34
Iamlixinzhao <150439580+Iamlixinzhao@users.noreply.github.com>
2025-05-28 10:24:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from lefdef import C_LefReader
 from lefdef import C_DefReader
-
+#change the file address below
 lef_reader = C_LefReader()
 _lef = lef_reader.read("external/def/TEST/complete.5.8.def")
 _lef.print()
